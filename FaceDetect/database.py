24707df3a5c6fef187935f6d902bf6128bd11d3f
IllidanSR <SergeiMatyshev@hotmail.com>
2018-04-28 15:51:14 +0300

    Add DB and read/write module

---
+++ FaceDetect/database.py
@@ -0,0 +1,36 @@
+import sqlite3
+import face_recognition
+import json
+class DB:
+    def __init__(self):
+        self.conn = sqlite3.connect('database.db')
+        self.cur = self.conn.cursor()
+        # self.cur = self.conn.execute("CREATE TABLE HUMAN(id CHAR, array ENCODER)")
+
+    def write_2_db(self,image):
+        my_face = face_recognition.load_image_file(image)
+        my_face_encoding = face_recognition.face_encodings(my_face)[0]
+        self.cur.execute("INSERT INTO HUMAN VALUES (?,?)", (17081993, json.dumps(my_face_encoding.tolist())))
+        self.conn.commit()
+        data = self.cur.execute("SELECT * FROM HUMAN")
+        data = self.cur.fetchall()
+        print(my_list)
+        # print(type(my_face_encoding))
+    def write(self, image):
+        my_face = face_recognition.load_image_file(image)
+        my_face_encoding = face_recognition.face_encodings(my_face)[0]
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO HUMAN VALUES (?,?)",(17081993,my_face_encoding))
+        conn.commit()
+        conn.close()
+    def read(self):
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM  HUMAN")
+        result = c.fetchall()
+        conn.close()
+        print(result[0][0])
+if __name__ == "__main__":
+    db = DB()
+    db.read()
